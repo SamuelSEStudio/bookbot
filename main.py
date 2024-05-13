aa30414a7fac85d38557953179e6cbf756802b9d
SamuelSEStudio <samuel@sestudios.co.nz>
2024-05-13 12:01:10 +1000

    updated main with count leetrs and updated code to be more readable

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ def main():
     book_path = "books/frankenstien.txt"
     text = get_book_text(book_path)
     word_count = count_words(text)
+    letter_count = count_letters(text)
     print(f"{word_count} words found in the {book_path}")
+    print(f"{letter_count}")
         
 def count_words(text):
     words = text.split()
     return len(words)
 
 def count_letters(text):
-    
+    letter_dict = {}
+    for character in text.lower():
+        if character not in letter_dict:
+            letter_dict[character] = 1
+        else:
+            letter_dict[character] += 1
+    return letter_dict
+ 
 
 def get_book_text(path):
     with open(path) as f:
