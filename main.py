45bda6621a9fa6eadc7c26c918731dc6d74eea4d
SamuelSEStudio <samuel@sestudios.co.nz>
2024-05-14 12:56:43 +1000

    rearranged functions and names started adding sort function

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ def main():
     text = get_book_text(book_path)
     word_count = count_words(text)
     letter_count = count_letters(text)
+    letter_count_dict = [{"char": char, "count": count} for char, count in letter_count.items() if char,isalpha()]
+    letter_count_dict.sort(reverse=True, key = sort_on)
     print(f"{word_count} words found in the {book_path}")
-    print(f"{letter_count}")
+    print(f"{letter_count_dict}")
         
 def count_words(text):
     words = text.split()
@@ -18,7 +20,12 @@ def count_letters(text):
         else:
             letter_dict[character] += 1
     return letter_dict
- 
+
+def sort_on(letter_count):
+    if letter_count["char"].isalpha():
+        return letter_count["count"]
+    else: 
+        return 0
 
 def get_book_text(path):
     with open(path) as f:
